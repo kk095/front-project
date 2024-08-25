bc9d058937b19453906445da0f172d59e07e4f12
krishan kant sharma <sharmakrishankant095@gmail.com>
2024-08-25 09:07:29 +0530

    adding core service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CoreDataService } from './Service/core-data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+
+
+  constructor(private coreDataService: CoreDataService){
+
+  }
+
+
+  public apiCall(){
+    this.coreDataService.getItem().subscribe((res)=>{
+      console.log(res);
+    })
+  }
+
 }
