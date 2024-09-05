81fe9bc87d12c89c9a0bda6a2dd743e7eaff7ee5
krishan kant sharma <sharmakrishankant095@gmail.com>
2024-09-05 22:21:18 +0530

    some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreDataService } from './Service/core-data.service';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
 import {RouterModule} from '@angular/router';
@@ -44,7 +43,6 @@ import { firebaseConfig } from 'src/firebaseconfig';
   
   ],
   providers: [
-    CoreDataService,
   ],
   bootstrap: [AppComponent]
 })
